06693f926c68931e329c6451913fd52f915bad27
takayamaroso <kd1242223@st.kobedenshi.ac.jp>
2018-01-09 13:49:42 +0900

    delete

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\mori keisuke\documents\djangoprojects\youwrite\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
