8edbaf359553b08f0e1da9ee19270108f242e39c
takayamaroso <kd1242223@st.kobedenshi.ac.jp>
2018-01-18 14:34:10 +0900

    settings.pyのMEDIA_ROOTを少し変更、no-image.jpgを変更

--- YouWrite/settings.py
+++ YouWrite/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR)
 
 LOGIN_REDIRECT_URL = '/'
