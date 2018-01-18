6dce27e885461ef7eb0a29129ac05327b932526f
takayamaroso <kd1242223@st.kobedenshi.ac.jp>
2018-01-18 14:37:11 +0900

    settings.pyのMEDIA_ROOTちょい変更

--- YouWrite/settings.py
+++ YouWrite/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = '/'
