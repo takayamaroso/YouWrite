9c802592c7719a2107894c6629a67ef1817a99f4
takayamaroso <kd1242223@st.kobedenshi.ac.jp>
2018-01-23 10:39:32 +0900

    DEBUGをFalseに変更

--- YouWrite/settings.py
+++ YouWrite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wmzo&zfa5&$6pv77w^%t(z48qfmf#of^e90o$nmkj%471fpr8r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
